d746c693bde254ccb37a86abad7e754aa057e037
happitec32101 <84065641+happitec32101@users.noreply.github.com>
2021-05-31 17:47:16 +0900

    implement events index

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import _ from 'lodash'
 
-import { increment, decrement } from '../actions'
+import { readEvents } from '../actions'
 
 class EventsIndex extends Component {
   componentDidMount() {
-    console.log("Hi!")
     this.props.readEvents()
   }
 
-  render() {
-    const props = this.props
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
 
+  render() {
     return (
-      <React.Fragment>
-        <div>value: { props.value }</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
     )
   } 
 }
 
-const mapStateToProps = state => ({ value: state.count.value })
+const mapStateToProps = state => ({ events: state.events })
 
 const mapDispatchToProps = ({ readEvents })
 
